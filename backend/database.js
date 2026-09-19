cb25d8cee4d4868093f8a6c6c4394d4e413ad204
Ashishkumar@0315 <ashishkumar0315d@gmail.com>
2026-09-19 07:26:49 +0530

    Connect backend to Railway MySQL

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.DB_PASSWORD,
-    database: "skillconnect"
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 db.connect((err) => {
